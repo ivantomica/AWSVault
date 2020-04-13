063858e7c7c4e32c8fec5c7f21fd21d29954371f
Ivan Tomica <ivan@tomica.net>
2020-04-13 09:45:47 +0200

    Initial PowerShell module improt

---
+++ Amazon-Web-Login.psm1
@@ -0,0 +1,31 @@
+<#
+    .Synopsis
+    Log-in into AWS Account with aws-vault utility and opet session in container tab
+
+    .Description
+    Log-in into AWS Account with aws-vault utility and opet session in container tab
+
+    .Parameter ProfileName
+    Name of the aws-vault profile
+
+    .Example
+    # Login to the AWS profile "example"
+    Amazon-Web-Login example
+
+    
+#>
+
+function Amazon-Web-Login {
+    param(
+        [parameter(Mandatory=$true)][string]$ProfileName
+    )
+
+    $aws_vault_login_stdout = (aws-vault login --stdout $ProfileName)
+    $aws_vault_login_url = $aws_vault_login_stdout.replace("&", "%26")
+
+    & "C:\Program Files\Mozilla Firefox\firefox.exe" "ext+container:name=$ProfileName&url=$aws_vault_login_url"
+}
+
+New-Alias -Name awl -Value Amazon-Web-Login -Scope Global
+
+Export-ModuleMember -Function Amazon-Web-Login
