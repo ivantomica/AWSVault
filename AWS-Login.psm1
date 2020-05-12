bbdfaf165f2c7f3194220875ec28a52930410e20
Ivan Tomica <ivan@tomica.net>
2020-05-12 18:22:27 +0200

    Module rework
    - rename functions
    - abstract session creation to separate function
    - open session in default browser by default
    - add switch to open in Firefox Container
    - add switch to override Firefox Containerr name

---
+++ AWS-Login.psm1
@@ -0,0 +1,49 @@
+<#
+    .Synopsis
+    Log-in into AWS Account with aws-vault utility and opet session in container tab
+
+    .Description
+    Log-in into AWS Account with aws-vault utility and opet session in container tab
+
+    .Parameter ProfileName
+    Name of the aws-vault profile
+
+    .Parameter FirefoxContainer
+    Switch parameter to use Firefox Container feature. 
+    Usage requires following extension: https://addons.mozilla.org/en-US/firefox/addon/open-url-in-container/
+
+    .Parameter FirefoxContainerName
+    By default container name is the same as specified ProfileName. With this switch you can override the name of the container.
+
+    .Example
+    # Login to the AWS profile "example"
+    Open-AWSSession example
+#>
+
+function New-AWSSession {
+    param (
+        [parameter(Mandatory=$true)][string]$ProfileName
+    )
+    
+    $script:AWSLoginURL = (aws-vault login --stdout $ProfileName)
+}
+
+function Open-AWSSession {
+    param(
+        [parameter(Mandatory=$true)][string]$ProfileName,
+        [switch]$FirefoxContainer,
+        [string]$FirefoxContainerName = $ProfileName
+    )
+
+    Create-AWSSession -ProfileName $ProfileName
+    
+    if ($FirefoxContainer -eq $true) {
+        $EscapedAWSLoginURL = $AWSLoginURL.Replace("&", "%26")
+        & "C:\Program Files\Mozilla Firefox\firefox.exe" "ext+container:name=$FirefoxContainerName&url=$EscapedAWSLoginURL"
+    }
+    else {
+        Start-Process -FilePath "$AWSLoginURL"
+    }
+}
+
+Export-ModuleMember -Function Open-AWSSession
